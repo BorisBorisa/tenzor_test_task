9e5b65dead51ae05269c676243559b5df21f3dcb
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-12-21 19:47:33 +0300

    test: add second scenario test

--- tests/test_task.py
+++ tests/test_task.py
@@ -6,6 +6,11 @@ from pages.tensor.about_page import TensorAboutPage
 
 from tools.enums.urls import URLs
 
+from config import settings
+
+test_data = settings.test_data
+
+
 class TestTask:
     def test_first_scenario(
             self,
@@ -32,8 +37,31 @@ class TestTask:
         tensor_about_page.check_work_block_visible()
         tensor_about_page.check_work_block_images_have_equal_dimensions()
 
-    def test_second_scenario(self):
-        pass
+    def test_second_scenario(
+            self,
+            saby_main_page: SabyMainPage,
+            saby_contacts_page: SabyContactsPage
+    ):
+        saby_main_page.visit(URLs.SABY_URL)
+        saby_main_page.is_page_opened()
+
+        saby_main_page.contacts_button_click()
+        saby_main_page.contacts_popup_menu.check_visible()
+        saby_main_page.contacts_popup_menu.click_contacts_link()
+
+        saby_contacts_page.is_page_opened()
+        saby_contacts_page.check_region_chooser_link_text(test_data.initial_region)
+
+        partners_for_initial_region = saby_contacts_page.get_partners_by_city()
+
+        saby_contacts_page.region_chooser_link_click()
+        saby_contacts_page.region_selection_dialog.check_visible()
+        saby_contacts_page.region_selection_dialog.region_item_link_click(test_data.new_region)
+
+        saby_contacts_page.check_region_chooser_link_text(test_data.new_region)
+        saby_contacts_page.check_has_partners_changed(partners_for_initial_region)
+        saby_contacts_page.check_current_url(test_data.expected_url_part)
+        saby_contacts_page.check_title_contains(test_data.expected_region_title_part)
 
     def test_third_scenario(self):
         pass
