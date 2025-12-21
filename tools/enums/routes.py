edd185e807c50d591636c01cf8e22c3bcf803f45
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-12-21 11:38:17 +0300

    feat: add enums for URLs and routes

---
+++ tools/enums/routes.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Routes(str, Enum):
+    ABOUT = "about"
+    CONTACT = "contacts"
+    DOWNLOAD = "download"
+
+    @property
+    def regex_pattern(self) -> str:
+        return rf'.*/{self.value}($|/.*)'
---
+++ tools/enums/urls.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class URLs(str, Enum):
+    SABY_URL = "https://saby.ru/"
+    TENSOR_URL = "https://tensor.ru/"
