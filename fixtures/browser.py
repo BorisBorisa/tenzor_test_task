208e78685354a085f5a6e17714aaf95d7b4fae0b
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-12-22 08:01:07 +0300

    feat: add tmp_path to driver fixture for default download folder

--- fixtures/browser.py
+++ fixtures/browser.py
@@ -1,5 +1,6 @@
 import pytest
 from _pytest.fixtures import SubRequest
+from pathlib import Path
 
 from tools.webdriver.browser import get_browser_driver
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -8,7 +9,7 @@ from config import settings
 
 
 @pytest.fixture(autouse=True, params=settings.browsers)
-def driver(request: SubRequest) -> WebDriver:
-    driver = get_browser_driver(request.param)
+def driver(request: SubRequest, tmp_path: Path) -> WebDriver:
+    driver = get_browser_driver(request.param, tmp_path)
     yield driver
     driver.quit()
