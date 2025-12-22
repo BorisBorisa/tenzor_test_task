b81e6aa4d070538ad54a10110c1c700b518b0284
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-12-22 12:09:46 +0300

    refactor: simplify driver fixture to use Chrome only

--- fixtures/browser.py
+++ fixtures/browser.py
@@ -1,15 +1,12 @@
 import pytest
-from _pytest.fixtures import SubRequest
 from pathlib import Path
 
-from tools.webdriver.browser import get_browser_driver
+from tools.webdriver.browser import get_chrome_browser_driver
 from selenium.webdriver.remote.webdriver import WebDriver
 
-from config import settings
 
-
-@pytest.fixture(autouse=True, params=settings.browsers)
-def driver(request: SubRequest, tmp_path: Path) -> WebDriver:
-    driver = get_browser_driver(request.param, tmp_path)
+@pytest.fixture
+def driver(tmp_path: Path) -> WebDriver:
+    driver = get_chrome_browser_driver(tmp_path)
     yield driver
     driver.quit()
