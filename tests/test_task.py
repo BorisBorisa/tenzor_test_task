14b89d76763367721d3733ec3bd16523653e99a9
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-12-22 08:04:47 +0300

    test: add third scenario test

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,10 +1,13 @@
+from pathlib import Path
+
 from pages.saby.main_page import SabyMainPage
 from pages.saby.contacts_page import SabyContactsPage
-
+from pages.saby.download_page import SabyDownloadPage
 from pages.tensor.main_page import TensorMainPage
 from pages.tensor.about_page import TensorAboutPage
 
 from tools.enums.urls import URLs
+from tools.assertions.files import assert_file_size
 
 from config import settings
 
@@ -63,5 +66,25 @@ class TestTask:
         saby_contacts_page.check_current_url(test_data.expected_url_part)
         saby_contacts_page.check_title_contains(test_data.expected_region_title_part)
 
-    def test_third_scenario(self):
-        pass
+    def test_third_scenario(
+            self,
+            saby_main_page: SabyMainPage,
+            saby_download_page: SabyDownloadPage,
+            tmp_path: Path
+    ):
+        saby_main_page.visit(URLs.SABY_URL)
+        saby_main_page.is_page_opened()
+
+        saby_main_page.footer.click_download_local_versions_link()
+
+        saby_download_page.is_page_opened()
+
+        saby_download_page.saby_desktop_tab_button_click()
+        saby_download_page.saby_desktop_windows_tab_button_click()
+        saby_download_page.download_saby_desktop_for_windows(tmp_path, test_data.saby_desktop_for_windows_app_file_name)
+
+        assert_file_size(
+            tmp_path,
+            test_data.saby_desktop_for_windows_app_file_name,
+            test_data.saby_desktop_for_windows_app_size
+        )
